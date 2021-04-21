0868393ba741088e569a276b5eec4cd63672b2e0
DAYANA <DAYANA@DESKTOP-OK7411E>
2021-04-20 21:57:45 -0500

    Lp2softRRHH

---
+++ ProyectosJAVA/lp2softRRHHModel/src/pe/edu/pucp/lp2soft/rrhh/model/Empleado.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.pucp.lp2soft.rrhh.model;
+
+/**
+ *
+ * @author DAYANA
+ */
+public class Empleado extends Persona {
+    private String cargo;
+    private double sueldo;
+    private Area area;
+
+    public Empleado(int idPersona, String nombreCompleto, String DNI,
+            String cargo, double sueldo) {
+        super(idPersona, nombreCompleto, DNI);
+        this.cargo = cargo;
+        this.sueldo = sueldo;
+    }
+
+    
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }
+    
+    
+}
